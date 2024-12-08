e537f483b18cf5c29b7ab9dfdf88910768456ca3
JD3M0N <josuedeki21@gmail.com>
2024-12-08 11:34:35 -0500

    working on user and student tables

--- AutomaticExamGeneration/Controllers/StudentController.cs
+++ AutomaticExamGeneration/Controllers/StudentController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Application.Interfaces;
 using System.Threading.Tasks;
+using Application.Dtos; 
+using Domain.Entities;
 
 namespace Web.Controllers
 {
@@ -9,10 +11,12 @@ namespace Web.Controllers
     public class StudentController : ControllerBase
     {
         private readonly IStudentService _studentService;
+        private readonly IUserService _userService;
 
-        public StudentController(IStudentService studentService)
+        public StudentController(IStudentService studentService, IUserService userService)
         {
             _studentService = studentService;
+            _userService = userService;
         }
 
         [HttpGet]
@@ -21,6 +25,32 @@ namespace Web.Controllers
             var students = await _studentService.GetStudentsAsync();
             return Ok(students);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddStudent([FromBody] StudentDto studentDto)
+        {
+            if (studentDto == null)
+            {
+                return BadRequest("Invalid student data.");
+            }
+
+            // Create User
+            var user = await _userService.AddUserAsync(studentDto.Name, studentDto.LastName, studentDto.Email, studentDto.Password, "Student");
+
+            // Create Student
+            var student = new Student
+            {
+                Id = user.Id,
+                Name = $"{studentDto.Name} {studentDto.LastName}",
+                Age = studentDto.Age,
+                Grade = studentDto.Grade
+            };
+
+            await _studentService.AddStudentAsync(student);
+
+            return Ok(student);
+        }
+
 
         [HttpDelete("clear")]
         public async Task<IActionResult> ClearStudents()
